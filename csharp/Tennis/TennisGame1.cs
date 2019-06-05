9529e14ec95a93447be7990b08152ca4e72396c4
chernetsova tatyana <t.chernetsova@dodopizza.com>
2019-06-05 16:50:41 +0300

    refactoring

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -8,14 +8,18 @@ namespace Tennis
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
+            {
                 _mScore1 += 1;
+            }
             else
+            {
                 _mScore2 += 1;
+            }
         }
 
         public string GetScore()
         {
-            string score = "";
+            var score = "";
             if (_mScore1 == _mScore2)
             {
                 switch (_mScore1)
@@ -32,24 +36,43 @@ namespace Tennis
                     default:
                         score = "Deuce";
                         break;
-
                 }
             }
             else if (_mScore1 >= 4 || _mScore2 >= 4)
             {
                 var minusResult = _mScore1 - _mScore2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                if (minusResult == 1)
+                {
+                    score = "Advantage player1";
+                }
+                else if (minusResult == -1)
+                {
+                    score = "Advantage player2";
+                }
+                else if (minusResult >= 2)
+                {
+                    score = "Win for player1";
+                }
+                else
+                {
+                    score = "Win for player2";
+                }
             }
             else
             {
                 for (var i = 1; i < 3; i++)
                 {
                     int tempScore;
-                    if (i == 1) tempScore = _mScore1;
-                    else { score += "-"; tempScore = _mScore2; }
+                    if (i == 1)
+                    {
+                        tempScore = _mScore1;
+                    }
+                    else
+                    {
+                        score += "-";
+                        tempScore = _mScore2;
+                    }
+
                     switch (tempScore)
                     {
                         case 0:
@@ -67,8 +90,8 @@ namespace Tennis
                     }
                 }
             }
+
             return score;
         }
     }
 }
-
