c019165042d4320a0f79be629452ea17726acfc6
chernetsova tatyana <t.chernetsova@dodopizza.com>
2019-06-05 17:28:57 +0300

    braces

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -19,24 +19,43 @@ namespace Tennis
             if (_p1Point == _p2Point && _p1Point < 3)
             {
                 if (_p1Point == 0)
+                {
                     score = "Love";
+                }
+
                 if (_p1Point == 1)
+                {
                     score = "Fifteen";
+                }
+
                 if (_p1Point == 2)
+                {
                     score = "Thirty";
+                }
+
                 score += "-All";
             }
             if (_p1Point == _p2Point && _p1Point > 2)
+            {
                 score = "Deuce";
+            }
 
             if (_p1Point > 0 && _p2Point == 0)
             {
                 if (_p1Point == 1)
+                {
                     _p1Res = "Fifteen";
+                }
+
                 if (_p1Point == 2)
+                {
                     _p1Res = "Thirty";
+                }
+
                 if (_p1Point == 3)
+                {
                     _p1Res = "Forty";
+                }
 
                 _p2Res = "Love";
                 score = _p1Res + "-" + _p2Res;
@@ -44,11 +63,19 @@ namespace Tennis
             if (_p2Point > 0 && _p1Point == 0)
             {
                 if (_p2Point == 1)
+                {
                     _p2Res = "Fifteen";
+                }
+
                 if (_p2Point == 2)
+                {
                     _p2Res = "Thirty";
+                }
+
                 if (_p2Point == 3)
+                {
                     _p2Res = "Forty";
+                }
 
                 _p1Res = "Love";
                 score = _p1Res + "-" + _p2Res;
@@ -57,25 +84,49 @@ namespace Tennis
             if (_p1Point > _p2Point && _p1Point < 4)
             {
                 if (_p1Point == 2)
+                {
                     _p1Res = "Thirty";
+                }
+
                 if (_p1Point == 3)
+                {
                     _p1Res = "Forty";
+                }
+
                 if (_p2Point == 1)
+                {
                     _p2Res = "Fifteen";
+                }
+
                 if (_p2Point == 2)
+                {
                     _p2Res = "Thirty";
+                }
+
                 score = _p1Res + "-" + _p2Res;
             }
             if (_p2Point > _p1Point && _p2Point < 4)
             {
                 if (_p2Point == 2)
+                {
                     _p2Res = "Thirty";
+                }
+
                 if (_p2Point == 3)
+                {
                     _p2Res = "Forty";
+                }
+
                 if (_p1Point == 1)
+                {
                     _p1Res = "Fifteen";
+                }
+
                 if (_p1Point == 2)
+                {
                     _p1Res = "Thirty";
+                }
+
                 score = _p1Res + "-" + _p2Res;
             }
 
@@ -89,14 +140,15 @@ namespace Tennis
                 score = "Advantage player2";
             }
 
-            if (_p1Point >= 4 && _p2Point >= 0 && (_p1Point - _p2Point) >= 2)
+            if (_p1Point >= 4 && _p2Point >= 0 && _p1Point - _p2Point >= 2)
             {
                 score = "Win for player1";
             }
-            if (_p2Point >= 4 && _p1Point >= 0 && (_p2Point - _p1Point) >= 2)
+            if (_p2Point >= 4 && _p1Point >= 0 && _p2Point - _p1Point >= 2)
             {
                 score = "Win for player2";
             }
+            
             return score;
         }
 
@@ -113,11 +165,14 @@ namespace Tennis
         public void WonPoint(string player)
         {
             if (player == "player1")
+            {
                 P1Score();
+            }
             else
+            {
                 P2Score();
+            }
         }
-
     }
 }
 
