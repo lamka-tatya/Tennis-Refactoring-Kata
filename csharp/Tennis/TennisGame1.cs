2461eda5d6a13cadb36592c6dd14f895b320cff2
chernetsova tatyana <t.chernetsova@dodopizza.com>
2019-06-05 16:56:46 +0300

    refactoring introduse new methods

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,93 +2,115 @@ namespace Tennis
 {
     class TennisGame1 : ITennisGame
     {
-        private int _mScore1;
-        private int _mScore2;
+        private int _player1Score;
+        private int _player2Score;
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
             {
-                _mScore1 += 1;
+                _player1Score += 1;
             }
             else
             {
-                _mScore2 += 1;
+                _player2Score += 1;
             }
         }
 
         public string GetScore()
         {
-            var score = "";
-            if (_mScore1 == _mScore2)
+            if (_player1Score == _player2Score)
             {
-                switch (_mScore1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-                }
+                return GetScoreForDraw();
             }
-            else if (_mScore1 >= 4 || _mScore2 >= 4)
+            
+            if (_player1Score >= 4 || _player2Score >= 4)
             {
-                var minusResult = _mScore1 - _mScore2;
-                if (minusResult == 1)
+                return GetScoreForGreaterThen4Score();
+            }
+
+            return GetScoreForLessThen4Score();
+        }
+
+        private string GetScoreForLessThen4Score()
+        {
+            var score = "";
+            
+            for (var i = 1; i < 3; i++)
+            {
+                int tempScore;
+                
+                if (i == 1)
                 {
-                    score = "Advantage player1";
-                }
-                else if (minusResult == -1)
-                {
-                    score = "Advantage player2";
-                }
-                else if (minusResult >= 2)
-                {
-                    score = "Win for player1";
+                    tempScore = _player1Score;
                 }
                 else
                 {
-                    score = "Win for player2";
+                    score += "-";
+                    tempScore = _player2Score;
                 }
+
+                switch (tempScore)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+            }
+
+            return score;
+        }
+
+        private string GetScoreForGreaterThen4Score()
+        {
+            string score;
+            var minusResult = _player1Score - _player2Score;
+            if (minusResult == 1)
+            {
+                score = "Advantage player1";
+            }
+            else if (minusResult == -1)
+            {
+                score = "Advantage player2";
+            }
+            else if (minusResult >= 2)
+            {
+                score = "Win for player1";
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    int tempScore;
-                    if (i == 1)
-                    {
-                        tempScore = _mScore1;
-                    }
-                    else
-                    {
-                        score += "-";
-                        tempScore = _mScore2;
-                    }
+                score = "Win for player2";
+            }
 
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+            return score;
+        }
+
+        private string GetScoreForDraw()
+        {
+            string score;
+            switch (_player1Score)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
             }
 
             return score;
